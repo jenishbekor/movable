411f3f890fe0d9e3ce288a01969260cd061cdb28
Your Name <you@example.com>
2024-11-25 14:46:56 +0600

    car file is changed

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -4,6 +4,13 @@ public class Car implements Movable{
 
     private String model;
 
+    public Car() {
+    }
+
+    public Car(String model) {
+        this.model = model;
+    }
+
     public void honk(){
         System.out.println("Car is honking the horn.");
     }
